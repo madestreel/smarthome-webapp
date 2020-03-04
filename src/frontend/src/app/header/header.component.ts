abaefcd1dce5e2d7e3d68262adb717479fa4066f
Maxime de Streel <maxime.destreel@student.uclouvain.be>
2020-03-04 16:01:45 +0100

    Add header

---
+++ src/frontend/src/app/header/header.component.ts
@@ -0,0 +1,18 @@
+import {Component} from "@angular/core";
+import {RoutesConfig} from "../configs/routes.config";
+import {AuthenticationService} from "../services/authentication.service";
+
+@Component({
+  selector: "app-header",
+  templateUrl: "./header.component.html",
+  styleUrls: [
+    './header.component.scss'
+  ]
+})
+
+export class HeaderComponent {
+  routes = RoutesConfig.routesName;
+
+  constructor(public auth: AuthenticationService) {
+  }
+}
