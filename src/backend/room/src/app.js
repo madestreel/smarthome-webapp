599dfc7556e69401edfb1104b6478b377bf54236
Maxime de Streel <maxime.destreel@student.uclouvain.be>
2020-04-04 12:53:12 +0200

    backend room logic
    - create/delete:
      - user to room
      - fav room for user
      - room
    
    - isfav -> is a room favorite for a user?
    
    TODO: delete room --> delete all owners / fav of the room
    TODO: delete room --> delete all cc for this room (later)
    TODO: room should exist when adding room to fav or adding user to room

--- src/backend/room/src/app.js
+++ src/backend/room/src/app.js
@@ -25,32 +25,36 @@ function isConnected(token) {
   })
 }
 
+function isValidRoom(room) {
+  return room.hasOwnProperty('permission')
+}
+
 /**
  * API to get a room
  * request params:
- *  - room: _
+ *  - roomID: _
  *
  * request query:
  *  - token: _
  *
  *  @param {String} token the token to be checked
- *  @param {String} room the room to retrieve
+ *  @param {String} roomID the room id to retrieve
  *
  *  @returns {status code} 200 in case of success, 400 in case of missing params,
  *                         err.message in case of invalid token and 500 otherwise.
  *  @returns {Room} In case of success the room is returned.
  *
- *  @see valid_room for the representation of the Room object.
+ *  @see isValidRoom for the representation of the Room object.
  */
-app.get('/room/:room', (req, res) => {
-  if(!(req.params.hasOwnProperty('room') && req.query.hasOwnProperty('token'))) {
+app.get('/room/:roomID', (req, res) => {
+  if(!(req.params.hasOwnProperty('roomID') && req.query.hasOwnProperty('token'))) {
     return res.status(400).json({status: 'invalid request'})
   }
-  let room = req.params.room;
+  let room = req.params.roomID;
   let token = req.query.token;
   isConnected(token).then(_ => {
     return db.getRoom(room).then(room => {
-      res.status(200).json({room:room})
+      res.status(200).json({room:room.room})
     }).catch(err => {
       res.status(500).json({message:String(err)})
     })
@@ -73,11 +77,11 @@ app.get('/room/:room', (req, res) => {
  *  @returns {status code} 200 in case of success, 400 in case of missing params,
  *                        err.message in case of invalid token and 500 otherwise.
  *
- *  @returns {List[Room]} In case of success the rooms of the user
+ *  @returns {List[string]} In case of success the ids of the rooms for the user
  *
- *  @see valid_room for the representation of the Room object.
+ *  @see isValidRoom for the representation of the Room object.
  */
-app.get('room/:user', (req, res) => {
+app.get('/rooms/:user', (req, res) => {
   if (!(req.params.hasOwnProperty('user') && req.query.hasOwnProperty('token'))) {
     return res.status(400).json({status: 'invalid request'})
   }
@@ -94,6 +98,208 @@ app.get('room/:user', (req, res) => {
   })
 });
 
+
+/**
+ * API to create a new room
+ * request body:
+ *  - room:
+ *    - permission: _
+ *  - token: _
+ *
+ *  @param {String} token the token to be checked
+ *  @param {Room} room the room to be created
+ *
+ *  @returns {status code} 200 in case of success, 400 in case of missing params,
+ *                        err.message in case of invalid token and 500 otherwise.
+ *
+ *  @see isValidRoom for the representation of the Room object.
+ */
+app.post('/room', (req, res) => {
+  log(req.body);
+  console.log(req.body);
+  if (!(req.body.hasOwnProperty('token') && req.body.hasOwnProperty('room') && isValidRoom(req.body.room))) {
+    res.status(400).json({message: 'bad request'});
+    return
+  }
+
+  const token = req.body.token;
+  const room = req.body.room;
+
+  isConnected(token).then(_ => {
+    return db.createRoom(room).then(_ => {
+      res.status(200).json({message: 'room successfully created'})
+    }).catch(err => {
+      res.status(500).json({message: err.message})
+    })
+  }).catch(err => {
+    res.status(err.message).json({message: 'permission denied'})
+  })
+});
+
+
+/**
+ * API to add a user to a room
+ * request body:
+ *  - roomID:
+ *  - userID: _
+ *  - token: _
+ *
+ *  @param {String} token the token to be checked
+ *  @param {String} roomID the room id where to user will be added
+ *  @param {String} userID the user id of the user to be added to the room
+ *
+ *  @returns {status code} 200 in case of success, 400 in case of missing params,
+ *                        err.message in case of invalid token and 500 otherwise.
+ *
+ */
+app.post('/user', (req, res) => {
+  if (!(req.body.hasOwnProperty('token') && req.body.hasOwnProperty('userID') && req.body.hasOwnProperty('roomID'))) {
+    res.status(400).json({message: 'bad request'});
+    return
+  }
+
+  const token = req.body.token;
+  const userID = req.body.userID;
+  const roomID = req.body.roomID;
+  isConnected(token).then(_ => {
+    return db.addUserToRoom(userID, roomID).then(_ => {
+      res.status(200).json({message: `user (${userID}) successfully added to room (${roomID})`})
+    }).catch(err => {
+      res.status(500).json({message: err.message})
+    })
+  }).catch(err => {
+    res.status(err.message).json({message: 'permission denied'})
+  });
+});
+
+
+app.delete('/user', (req, res) => {
+  if (!(req.body.hasOwnProperty('token') && req.body.hasOwnProperty('userID') && req.body.hasOwnProperty('roomID'))) {
+    res.status(400).json({message: 'bad request'})
+    return
+  }
+
+  const token = req.body.token;
+  const userID = req.body.userID;
+  const roomID = req.body.roomID;
+
+  isConnected(token).then(_ => {
+    return db.deleteUserOfRoom(userID, roomID).then(_ => {
+      res.status(200).json({message: 'success'})
+    }).catch(err => {
+      res.status(500).json({message: err.message})
+    })
+  }).catch(err => {
+    res.status(err.message).json({message: 'permission denied'})
+  })
+});
+
+/**
+ * API to add a room to the favs of a user
+ * request body:
+ *  - roomID:
+ *  - userID: _
+ *  - token: _
+ *
+ *  @param {String} token the token to be checked
+ *  @param {String} roomID the room id where to user will be added
+ *  @param {String} userID the user id of the user to be added to the room
+ *
+ *  @returns {status code} 200 in case of success, 400 in case of missing params,
+ *                        err.message in case of invalid token and 500 otherwise.
+ *
+ */
+app.post('/fav', (req, res) => {
+  if (!(req.body.hasOwnProperty('token') && req.body.hasOwnProperty('userID') && req.body.hasOwnProperty('roomID'))) {
+    res.status(400).json({message: 'bad request'});
+    return
+  }
+
+  const token = req.body.token;
+  const userID = req.body.userID;
+  const roomID = req.body.roomID;
+  isConnected(token).then(_ => {
+    return db.addFav(userID, roomID).then(_ => {
+      res.status(200).json({message: 'success'})
+    }).catch(err => {
+      res.status(500).json({message: err.message})
+    })
+  }).catch(err => {
+    res.status(err.message).json({message: 'permission denied'})
+  })
+});
+
+
+/**
+ * API to remove a room to the favs of a user
+ * request body:
+ *  - roomID: _
+ *  - userID: _
+ *  - token: _
+ *
+ *  @param {String} token the token to be checked
+ *  @param {String} roomID the room id where to user will be added
+ *  @param {String} userID the user id of the user to be added to the room
+ *
+ *  @returns {status code} 200 in case of success, 400 in case of missing params,
+ *                        err.message in case of invalid token and 500 otherwise.
+ *
+ */
+app.delete('/fav', (req, res) => {
+  if (!(req.body.hasOwnProperty('token') && req.body.hasOwnProperty('userID') && req.body.hasOwnProperty('roomID'))) {
+    res.status(400).json({message: 'bad request'});
+    return
+  }
+
+  const token = req.body.token;
+  const roomID = req.body.roomID;
+  const userID = req.body.userID;
+  isConnected(token).then(_ => {
+    return db.deleteFav(userID, roomID).then(_ => {
+      res.status(200).json({message: 'success'})
+    }).catch(err => {
+      res.status(500).json({message: err.message})
+    })
+  }).catch(err => {
+    res.status(err.message).json({message: 'permission denied'})
+  })
+});
+
+
+/**
+ * API to remove a user from a room
+ * request body:
+ *  - roomID: _
+ *  - userID: _
+ *  - token: _
+ *
+ *  @param {String} token the token to be checked
+ *  @param {String} roomID the room id where to user will be added
+ *  @param {String} userID the user id of the user to be added to the room
+ *
+ *  @returns {status code} 200 in case of success, 400 in case of missing params,
+ *                        err.message in case of invalid token and 500 otherwise.
+ *
+ */
+app.get('/fav/room/:roomID/user/:userID', (req, res) => {
+  if (!(req.params.hasOwnProperty('roomID') && req.params.hasOwnProperty('userID') && req.query.hasOwnProperty('token'))) {
+    res.status(400).json({message: 'bad request'});
+    return
+  }
+
+  const token = req.query.token;
+  const userID = req.params.userID;
+  const roomID = req.params.roomID;
+  isConnected(token).then(_ => {
+    db.isFav(userID, roomID).then(isfav => {
+      res.status(200).json({message: 'success', isfav: isfav})
+    }).catch(err => {
+      res.status(500).json({message: err.message})
+    })
+  }).catch(err => {
+    res.status(err.message).json({message: 'permission denied'})
+  })
+});
 
 app.get('/', (req, res) => {
   return res.status(200).json({status: "success"})
